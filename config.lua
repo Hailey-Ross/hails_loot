3f2dd5e632d80ee41cd5bcebdc7a96e421b34461
Hailey <26784904+Hailey-Ross@users.noreply.github.com>
2023-05-15 02:01:20 -0600

    commenting

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ Config = {}
     Config.debug = false           -- True enables debug output | False for opposite
 
     Config.discordToggle = false  -- True enables Discord Webhooks | False to Disable
-    Config.discordWebhook = ""
+    Config.discordWebhook = ""    -- Paste your Discord Webhook here, Previous option must be enabled to work. 
 
     Config.LootingLow = 0         -- Set for low end of change dropped, usually 0 or 1
     Config.LootingHigh = 10       -- Set for high end of change dropped, usually 10 for $1.00 max dropped by a Ped

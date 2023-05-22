6f38d29d9cf889e6970862a2fdf59d32d01dec86
Hails <26784904+Hailey-Ross@users.noreply.github.com>
2023-05-22 02:59:52 -0600

    minor text formatting

--- config.lua
+++ config.lua
@@ -9,7 +9,7 @@ Config = {}
     Config.LootingLow = 0         -- Set for low end of change dropped, Default 0 or 1
     Config.LootingHigh = 10       -- Set for high end of change dropped, Default 10 for $1.00 max dropped by a Ped
 
-    Config.notifyLength = 3       -- Time in Seconds for the notification to appear
+    Config.notifyLength = 3       -- Time in seconds for the notification to appear
     Config.butterfingers = 'You watch as your fingers slip and the coins disappear into the dirt at your feet..'  -- Failure text if Loot Prompt isn't held to full length
     Config.searchFailtext = 'You search their pockets, however you find nothing of value..'
     Config.searchFindtext = 'You steal'
